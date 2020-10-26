fd370e77e1cf4c3ae345012df71e7441a3a8ebf5
ali punhal <alipunhal82@gmail.com>
2020-10-26 12:33:30 -0700

    initial commit

---
+++ js/question.js
@@ -0,0 +1,44 @@
+
+const quiz =[
+
+	{
+		q:'Which month comes right before june ?',
+		option:['may','september','july','august'],
+		answer:0
+	},
+	{
+		q:'What color is a banana?',
+		option:['red','yellow','white','blue'],
+		answer:1
+	},
+	{
+		q:'3 + 4 = 7 ?',
+		option:['true','false'],
+		answer:0
+	},
+
+	{
+		q:'What time of day do we have breakfast?',
+		option:['In the afternoon','In the evening','In the morning'],
+		answer:2
+	},
+	{
+		q:'What is 22 + 6?',
+		option:['99','56','16','28'],
+		answer:3
+	},
+	{
+		q:'What is the capital of India?',
+		option:['mumbai','delhi','mahraster','pone'],
+		answer:1
+	}
+
+		
+
+		]
+
+
+
+
+
+
